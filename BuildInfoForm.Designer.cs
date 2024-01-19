516d4d3702b25b025b31795112b851c0bde37caf
Yvan Njanko <yvannjanko04@gmail.com>
2024-01-19 12:50:47 +0100

    modification

--- BuildInfoForm.Designer.cs
+++ BuildInfoForm.Designer.cs
@@ -28,19 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BuildInfoForm));
+            dataGridViewBuildings = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewBuildings).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewBuildings
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(77, 95);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 57;
-            dataGridView1.RowTemplate.Height = 32;
-            dataGridView1.Size = new Size(927, 223);
-            dataGridView1.TabIndex = 0;
+            dataGridViewBuildings.BackgroundColor = Color.GhostWhite;
+            dataGridViewBuildings.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewBuildings.Location = new Point(381, 94);
+            dataGridViewBuildings.Name = "dataGridViewBuildings";
+            dataGridViewBuildings.RowHeadersWidth = 57;
+            dataGridViewBuildings.RowTemplate.Height = 32;
+            dataGridViewBuildings.Size = new Size(675, 404);
+            dataGridViewBuildings.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 26);
+            button1.TabIndex = 25;
+            button1.Text = "<";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Black;
+            button2.Font = new Font("Century Gothic", 9.134328F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(73, 356);
+            button2.Name = "button2";
+            button2.Size = new Size(222, 60);
+            button2.TabIndex = 26;
+            button2.Text = "Create New Building";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Black;
+            button3.Font = new Font("Century Gothic", 9.134328F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(73, 438);
+            button3.Name = "button3";
+            button3.Size = new Size(222, 60);
+            button3.TabIndex = 27;
+            button3.Text = "Delete Building";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(55, 143);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(271, 197);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 28;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 18.2686558F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(496, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(392, 40);
+            label2.TabIndex = 29;
+            label2.Text = "Manage Your Buildings";
             // 
             // BuildInfoForm
             // 
@@ -48,17 +112,29 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1085, 577);
-            Controls.Add(dataGridView1);
-            Font = new Font("Century", 9.134328F, FontStyle.Regular, GraphicsUnit.Point);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridViewBuildings);
+            Font = new Font("Century Gothic", 9.134328F, FontStyle.Regular, GraphicsUnit.Point);
             Name = "BuildInfoForm";
             Text = "BuildInfoForm";
             Load += BuildInfoForm_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewBuildings).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dataGridViewBuildings;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private Label label2;
     }
 }
--- BuildInfoForm.cs
+++ BuildInfoForm.cs
@@ -1,25 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
 
 namespace FrontEnd_Gestion_CiteU
 {
     public partial class BuildInfoForm : Form
     {
+        private MySqlConnection connection;
+        private string connectionString = "Server=localhost;Database=bdcite;User ID=root;Password=;";
+
         public BuildInfoForm()
         {
             InitializeComponent();
+            connection = new MySqlConnection(connectionString);
         }
 
         private void BuildInfoForm_Load(object sender, EventArgs e)
         {
-
+            // Charger les données des bâtiments depuis la base de données et les afficher dans le DataGridView
+            LoadBuildingData();
         }
+
+        private void LoadBuildingData()
+        {
+            try
+            {
+                // Ouvrir la connexion à la base de données
+                connection.Open();
+
+                // Sélectionner les données des bâtiments depuis la table Batiment
+                string query = "SELECT Code, NombreEtages, ChambresParEtage, PrixChambre, NombreMaxLitsParChambre FROM Batiment";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                // Utiliser un adaptateur de données pour remplir un DataSet
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet, "Batiment");
+
+                // Afficher les données dans le DataGridView
+                dataGridViewBuildings.DataSource = dataSet.Tables["Batiment"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des données des bâtiments : " + ex.Message);
+            }
+            finally
+            {
+                // Fermer la connexion à la base de données
+                connection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Menu1 menu = new Menu1();
+            menu.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Redirection vers la page CreateBuilding.cs
+            NewBuild createBuildingForm = new NewBuild();
+            createBuildingForm.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Demander le code du bâtiment à supprimer
+            string codeBatimentToDelete = Interaction.InputBox("Veuillez entrer le code du bâtiment à supprimer:", "Supprimer un bâtiment", "");
+
+            if (!string.IsNullOrEmpty(codeBatimentToDelete))
+            {
+                try
+                {
+                    // Ouvrir la connexion à la base de données
+                    connection.Open();
+
+                    // Supprimer les chambres associées au bâtiment
+                    string deleteChambresQuery = "DELETE FROM Chambre WHERE BatimentCode = @batimentCode";
+                    MySqlCommand deleteChambresCmd = new MySqlCommand(deleteChambresQuery, connection);
+                    deleteChambresCmd.Parameters.AddWithValue("@batimentCode", codeBatimentToDelete);
+                    deleteChambresCmd.ExecuteNonQuery();
+
+                    // Supprimer le bâtiment lui-même
+                    string deleteBatimentQuery = "DELETE FROM Batiment WHERE Code = @code";
+                    MySqlCommand deleteBatimentCmd = new MySqlCommand(deleteBatimentQuery, connection);
+                    deleteBatimentCmd.Parameters.AddWithValue("@code", codeBatimentToDelete);
+                    deleteBatimentCmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Bâtiment et chambres associées supprimés avec succès.");
+
+                    // Rafraîchir les données affichées dans le DataGridView
+                    LoadBuildingData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erreur lors de la suppression du bâtiment : " + ex.Message);
+                }
+                finally
+                {
+                    // Fermer la connexion à la base de données
+                    connection.Close();
+                }
+            }
+        }
+
     }
 }
